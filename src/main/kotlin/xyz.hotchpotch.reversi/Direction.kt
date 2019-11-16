8409fcc219b0ee69c72d10ce459e1a4ebff7779d
nmby <nmby@hotchpotch.xyz>
2019-11-17 03:13:51 +0900

    Direction.oppositeの追加

--- src/main/kotlin/xyz.hotchpotch.reversi/Direction.kt
+++ src/main/kotlin/xyz.hotchpotch.reversi/Direction.kt
@@ -6,29 +6,32 @@ package xyz.hotchpotch.reversi
  * @property di 縦方向のベクトル
  * @property dj 横方向のベクトル
  */
-enum class Direction(internal val di: Int, internal val dj: Int) {
+enum class Direction(internal val di: Int, internal val dj: Int, opposite: Lazy<Direction>) {
 
     /** 上 */
-    UPPER(-1, 0),
+    UPPER(-1, 0, lazy { LOWER }),
 
     /** 右上 */
-    UPPER_RIGHT(-1, 1),
+    UPPER_RIGHT(-1, 1, lazy { LOWER_LEFT }),
 
     /** 右 */
-    RIGHT(0, 1),
+    RIGHT(0, 1, lazy { LEFT }),
 
     /** 右下 */
-    LOWER_RIGHT(1, 1),
+    LOWER_RIGHT(1, 1, lazy { UPPER_LEFT }),
 
     /** 下 */
-    LOWER(1, 0),
+    LOWER(1, 0, lazy { UPPER }),
 
     /** 左下 */
-    LOWER_LEFT(1, -1),
+    LOWER_LEFT(1, -1, lazy { UPPER_RIGHT }),
 
     /** 左 */
-    LEFT(0, -1),
+    LEFT(0, -1, lazy { RIGHT }),
 
     /** 左上 */
-    UPPER_LEFT(-1, -1);
+    UPPER_LEFT(-1, -1, lazy { LOWER_RIGHT });
+
+    /** 自身と反対の方向 */
+    val opposite: Direction by opposite
 }
