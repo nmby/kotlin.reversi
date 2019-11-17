0f892fb97bc710da608f2fe47d6044b847a8053c
nmby <nmby@hotchpotch.xyz>
2019-11-17 16:47:43 +0900

    コメント修正

--- src/main/kotlin/xyz.hotchpotch.reversi/players/Utils.kt
+++ src/main/kotlin/xyz.hotchpotch.reversi/players/Utils.kt
@@ -10,7 +10,7 @@ fun stablePoints(board: Board): Set<Point> {
     // 安定であることが判明した石の位置を記録するセット
     val stablePoints: MutableSet<Point> = mutableSetOf()
 
-    // まだ安定か安定でないかが未判明の石の位置を記録するセット
+    // まだ安定か不安定かが未判明の石の位置を記録するセット
     val unclearPoints: MutableSet<Point> =
             Point.values.filter { board[it] !== null }.toMutableSet()
 
@@ -22,22 +22,24 @@ fun stablePoints(board: Board): Set<Point> {
         // 安定であることが判明したものは stablePoints に追加する。
         stablePoints.addAll(results.filter { it.value == true }.keys)
 
-        // 安定であるか安定でないかが判明したもの（つまり、引き続き未判明のもの以外）を unclearPoints から除く。
+        // 安定か不安定かが判明したもの（つまり、引き続き未判明のもの以外）を unclearPoints から除く。
         unclearPoints.removeIf { results[it] !== null }
     }
     return stablePoints
 }
 
 /**
- * 指定されたリバーシ盤上の指定された位置にある石が安定である場合に true を返します。
+ * 指定されたリバーシ盤上の指定された位置にある石が安定である場合に true を、
+ * 不安定である場合に false を、未判明である場合に null を返します。
  * 「安定である」とは、今後決してひっくり返されることがないことを意味します。
  *
  * 注意：
  * 現在の実装アルゴリズムには不完全な点があります。そのため、
- * 本来は安定である位置を安定でないと判断することがあります。
+ * 本来は安定である位置を不安定であると判断することがあります。
  *
  * @param board 検査時点のリバーシ盤
  * @param stablePoints これまでに判明している「安定な石」の場所
+ * @param unclearPoints 安定であるか不安定であるかが未判明の場所
  * @param testee 検査対象の位置
  */
 private fun isStable(
@@ -53,15 +55,20 @@ private fun isStable(
             .map { isStable(board, stablePoints, unclearPoints, testee, it) }
 
     return when {
+        // 全ての方向について安定であるならば、安定であると言える。
         isStableInEachDirection.all { it == true } -> true
+
+        // 不安定である方向が一つでもあるならば、不安定である。
         isStableInEachDirection.any { it == false } -> false
+
+        // 上記に当てはまらない場合は未判明である。
         else -> null
     }
 }
 
 /**
  * 指定されたリバーシ盤上の指定された位置にある石が、指定された方向について安定である場合に
- * true を返します。
+ * true を、不安定である場合に false を、未判明である場合に null を返します。
  * 「指定された方向について安定である」とは、具体的には次の条件を満たす場合を指します。
  *   - 指定された位置から指定された方向に向けて、開きマスが無い場合
  *   - 開きマスがある場合であっても、反対方向に向けて相手の石が無い、かつ、入り込む余地が無い場合
@@ -69,12 +76,12 @@ private fun isStable(
  *
  * 注意：
  * 現在の実装アルゴリズムには不完全な点があります。そのため、
- * 本来は安定である位置を安定でないと判断することがあります。
+ * 本来は安定である位置を不安定であると判断することがあります。
  * TODO: アルゴリズムを改善する（結構難しい）
  *
  * @param board 検査時点のリバーシ盤
  * @param stablePoints これまでに判明している「安定な石」の場所
- * @param unclearPoints 安定であるか否かが判定していない場所
+ * @param unclearPoints 安定であるか不安定であるかが未判明の場所
  * @param testee 検査対象の位置
  * @param direction 検査対象の方向
  */
@@ -99,7 +106,7 @@ private fun isStable(
     if (oppositeNext === null) return true
 
     // 反対方向に隣接するマスが自身の石でない場合（つまり、空もしくは相手の石の場合）は
-    // 「安定ではない」と判断する。
+    // 「不安定である」と判断する。
     if (board[oppositeNext] !== board[testee]) return false
 
     // 反対方向に隣接するマスが自身の石の場合
@@ -107,10 +114,10 @@ private fun isStable(
         // 隣接する石が安定であるなら、この石も安定である。
         stablePoints.contains(oppositeNext) -> true
 
-        // 隣接する石が安定であるか否か不明であるなら、この石についてもまだ判断は下せない。
+        // 隣接する石が安定であるか不安定であるか未判明であるなら、この石についても未判明である。
         unclearPoints.contains(oppositeNext) -> null
 
-        // 隣接する石が安定でないなら、この石も安定でない。
+        // 隣接する石が不安定であるなら、この石も不安定である。
         else -> false
     }
 }
